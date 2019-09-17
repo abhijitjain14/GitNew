ae68f642a0784bbc4dbb58f2dad4480ecc7ad5aa
jainabhijit <jainabhijit@GOLDROOM-40.takaful-ikhlas.com.my.local>
2019-09-17 12:39:04 +0800

    abhijit

--- Scripts/Tc2/Script1568692168334.groovy
+++ Scripts/Tc2/Script1568692168334.groovy
@@ -19,3 +19,4 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.comment('abhijit')
